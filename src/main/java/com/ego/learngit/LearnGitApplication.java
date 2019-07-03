4bf7a801d5d8fccece9f79c7d0a9c79bed2d03bd
易归霞 <yiguixia@giveu.cn>
2019-07-03 10:30:21 +0800

    Initial commit

---
+++ src/main/java/com/ego/learngit/LearnGitApplication.java
@@ -0,0 +1,13 @@
+package com.ego.learngit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnGitApplication.class, args);
+    }
+
+}
